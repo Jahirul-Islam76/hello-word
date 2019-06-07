0afd53464d45a9d4be2a3c3f46a23d917a3c2292
JAHIRUL ISLAM <41253629+Jahirul-Islam76@users.noreply.github.com>
2019-06-07 10:51:15 +0600

    initial code for me

---
+++ Wellcome in Helloc++.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>       // bits/stdc++.h header using for support both of them c and cpp
+
+using namespace std;
+
+int main()
+{
+    //if you want to print anything in c program please using printf function like-
+    printf("Hello World in C\n");       //my first program in c.
+    //else you want to print anything in c++ program please using cout funtion like-
+    cout<<"Hello World in CPP"<<endl;  //my first program in cpp.
+    cout<<"\n\nThank you for see my first program from github !"<<endl;
+    return 0;
+}
